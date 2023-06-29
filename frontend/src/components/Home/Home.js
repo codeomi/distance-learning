b90adbb89362e5021212a780a8c95e9bc94f1f39
Omkar Kudale <omkar7890kudale@gmail.com>
2023-06-29 17:04:37 +0530

    section 3 completed

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./home.scss"
 import Section1 from './Sections/Section1'
 import Section2 from './Sections/Section2.js'
+import Section3 from './Sections/Section3.js'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
   <div className="home">
  <Section1 />
  <Section2 />
+ <Section3 />
   </div>
    </>
   )
---
+++ frontend/src/components/Home/Sections/Section3.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./section3.scss"
+
+const Section3 = () => {
+  return (
+    <>
+      <div className="section3">
+        <div className="section-container">
+          <div className="section-description white">
+            <div className="white-heading-1 white">Ready to Take the Leap</div>
+            <div className="para-container">
+              Discover a world of academic possibilities with our amazing
+              college portal. Unearth top-notch institutions, reach for stars
+              and make your dreams come true!
+            </div>
+          </div>
+          <div className="counselling-btn">Get Free Career Counselling</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Section3;
