28173d874cd4ed3d12af9dc4a375f1a2bf0d39da
Omkar Kudale <omkar7890kudale@gmail.com>
2023-07-04 16:53:47 +0530

    section1 mobile view added

--- frontend/src/components/Layout/Navbar.js
+++ frontend/src/components/Layout/Navbar.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.scss";
 import { Link, useLocation, useParams } from "react-router-dom";
-
+import MenuIcon from "@mui/icons-material/Menu";
+import SimpliDistLogo from "../../assets/simpli-dist-logo.png";
+import { Drawer, List, ListItem, Menu, MenuItem, Popover } from "@mui/material";
 
 const Navbar = () => {
-  
   const location = useLocation();
 
   const isActiveLink = (link) => {
@@ -12,33 +13,60 @@ const Navbar = () => {
     return pathname === link ? "active" : "";
   };
 
-  console.log(location)
+
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+  console.log(location);
   return (
     <>
-        <div className="navbar">
-          <Link to="/" className="conatiner-1">
-            <img
-              src="https://simplidistance.com/wp-content/uploads/2022/07/sd-new-rounded-logo.png"
-              sizes="(max-width: 349px) 100vw, 349px"
-              alt=""
-            />
+      <div className="navbar">
+        <Link to="/" className="conatiner-1">
+          <img
+            src={SimpliDistLogo}
+            sizes="(max-width: 349px) 100vw, 349px"
+            alt=""
+          />
+        </Link>
+        <div className="conatiner-2">
+          <Link to="/" className={`navbar-text ${isActiveLink("/")}`}>
+            Home
           </Link>
-          <div className="conatiner-2">
-            <Link to="/"  className={`navbar-text ${isActiveLink("/")}`}>
-              Home
-            </Link>
-            <Link to="/about"  className={`navbar-text ${isActiveLink("/about")}`}>
-              About
-            </Link>
-            <Link to="/blogs"  className={`navbar-text ${isActiveLink("/blogs")}`}>
-              Blogs
-            </Link>
-            <Link to="/contact"  className={`navbar-text ${isActiveLink("/contact")}`}>
-              Contact
-            </Link>
-          </div>
+          <Link to="/about" className={`navbar-text ${isActiveLink("/about")}`}>
+            About
+          </Link>
+          <Link to="/blogs" className={`navbar-text ${isActiveLink("/blogs")}`}>
+            Blogs
+          </Link>
+          <Link
+            to="/contact"
+            className={`navbar-text ${isActiveLink("/contact")}`}
+          >
+            Contact
+          </Link>
         </div>
-     
+        <div className="mobile-display-menu">
+        
+        <MenuIcon id="menu-btn" onClick={toggleDrawer(true)} />
+          <Drawer
+            anchor="right"
+            open={isDrawerOpen}
+            onClose={toggleDrawer(false)}
+          >
+            <List>
+              <ListItem>Omkar</ListItem>
+            </List>
+          </Drawer>
+        </div>
+      </div>
     </>
   );
 };
---
+++ frontend/src/components/Layout/SwipableDrawer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SwipableDrawer = () => {
+  return (
+   <>
+   </>
+  )
+}
+
+export default SwipableDrawer
